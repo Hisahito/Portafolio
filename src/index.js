0c0756e44df3cae250a704ec20379a418c70b8ab
hisahito <hisahito706@gmail.com>
2025-02-22 19:46:12 -0700

    FIX IN index.js styles are now on, adding blog.png image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import { saludar } from './js/componentes';
-// import './style.css';
+ import { saludar } from './js/componentes';
+ import './style.css';
 /*
 const project = document.querySelector('#proyecto');
 
